994e2eb1aa9845794829491a0197b886ed2d15d3
kkuoii007-coder <kkuoii007@gmail.com>
2026-04-03 10:21:05 +0300

    final

--- FirstDjango/main/views.py
+++ FirstDjango/main/views.py
@@ -4,4 +4,4 @@ def data(request):
     return HttpResponse("<h1>Ув. Куратор! Давно хотел сказать:</h1>")
 
 def test(request):
-    return HttpResponse("<h1> Благодарю за обратную связь и потраченное время🤝</h1>")
+    return HttpResponse("<h1> Благодарю за обратную связь и потраченное время🤝!</h1>")
